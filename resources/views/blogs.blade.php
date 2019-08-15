17749bb3110bc9f37c376edaa62a3e673bc191fd
aostrovs <ostrovsky.aaron@gmail.com>
2019-08-15 19:41:33 -0400

    updated blogs.blade.php

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -3,6 +3,7 @@
 @include('partials.nav')
 <div class="container">
         <h1>Blogs</h1>
+        <h2>This should only be under development</h2>
     <div class="row">
 
     @foreach($blogs as $blog)
